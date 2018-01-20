767309bc93022fbb2337959edf0412f772863eb8
pausla <32725924+pausla@users.noreply.github.com>
2018-01-20 13:09:51 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oAutorzeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zamknijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 26);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(285, 173);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Wpisz tekst, który chcesz zamienić na kod Morse\'a";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(80, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Zamień";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oAutorzeToolStripMenuItem,
+            this.zamknijToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // oAutorzeToolStripMenuItem
+            // 
+            this.oAutorzeToolStripMenuItem.Name = "oAutorzeToolStripMenuItem";
+            this.oAutorzeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.oAutorzeToolStripMenuItem.Text = "O autorze";
+            this.oAutorzeToolStripMenuItem.Click += new System.EventHandler(this.oAutorzeToolStripMenuItem_Click);
+            // 
+            // zamknijToolStripMenuItem
+            // 
+            this.zamknijToolStripMenuItem.Name = "zamknijToolStripMenuItem";
+            this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.zamknijToolStripMenuItem.Text = "Zamknij";
+            this.zamknijToolStripMenuItem.Click += new System.EventHandler(this.zamknijToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oAutorzeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        static Dictionary<char, string> translator;
+        private static string translate(string input)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (char character in input)
+            {
+                if (translator.ContainsKey(character))
+                {
+                    sb.Append(translator[character] + " ");
+                }
+                else if (character == ' ')
+                {
+                    sb.Append("/ ");
+                }
+                else
+                {
+                    sb.Append(character + " ");
+                }
+            }
+            return sb.ToString();
+        }
+        private static void InitialiseDictionary()
+        {
+            char dot = '.';
+            char dash = '−';
+
+            translator = new Dictionary<char, string>()
+            {
+                {'a', string.Concat(dot, dash)},
+                {'b', string.Concat(dash, dot, dot, dot)},
+                {'c', string.Concat(dash, dot, dash, dot)},
+                {'d', string.Concat(dash, dot, dot)},
+                {'e', dot.ToString()},
+                {'f', string.Concat(dot, dot, dash, dot)},
+                {'g', string.Concat(dash, dash, dot)},
+                {'h', string.Concat(dot, dot, dot, dot)},
+                {'i', string.Concat(dot, dot)},
+                {'j', string.Concat(dot, dash, dash, dash)},
+                {'k', string.Concat(dash, dot, dash)},
+                {'l', string.Concat(dot, dash, dot, dot)},
+                {'m', string.Concat(dash, dash)},
+                {'n', string.Concat(dash, dot)},
+                {'o', string.Concat(dash, dash, dash)},
+                {'p', string.Concat(dot, dash, dash, dot)},
+                {'q', string.Concat(dash, dash, dot, dash)},
+                {'r', string.Concat(dot, dash, dot)},
+                {'s', string.Concat(dot, dot, dot)},
+                {'t', string.Concat(dash)},
+                {'u', string.Concat(dot, dot, dash)},
+                {'v', string.Concat(dot, dot, dot, dash)},
+                {'w', string.Concat(dot, dash, dash)},
+                {'x', string.Concat(dash, dot, dot, dash)},
+                {'y', string.Concat(dash, dot, dash, dash)},
+                {'z', string.Concat(dash, dash, dot, dot)},
+                {'0', string.Concat(dash, dash, dash, dash, dash)},
+                {'1', string.Concat(dot, dash, dash, dash, dash)},
+                {'2', string.Concat(dot, dot, dash, dash, dash)},
+                {'3', string.Concat(dot, dot, dot, dash, dash)},
+                {'4', string.Concat(dot, dot, dot, dot, dash)},
+                {'5', string.Concat(dot, dot, dot, dot, dot)},
+                {'6', string.Concat(dash, dot, dot, dot, dot)},
+                {'7', string.Concat(dash, dash, dot, dot, dot)},
+                {'8', string.Concat(dash, dash, dash, dot, dot)},
+                {'9', string.Concat(dash, dash, dash, dash, dot)}
+            };
+            }
+
+
+            public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitialiseDictionary();
+            string input;
+            input = richTextBox1.Text;
+            input = input.ToLower();
+            richTextBox1.Text= translate(input);
+            serialPort1 = new SerialPort();
+            serialPort1.PortName = "COM2";
+            serialPort1.BaudRate = 9600;
+            try
+            {
+                serialPort1.Open();
+                serialPort1.WriteLine(translate(input));
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show("Change your serialport's number in code");
+
+            }
+            serialPort1.Close();
+
+
+
+
+
+
+        }
+
+        private void oAutorzeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("by Paulina Sławińska");
+        }
+
+        private void zamknijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
